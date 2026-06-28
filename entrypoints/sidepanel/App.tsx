9ed7350ac88145c2e1386feb0c74e2bcbac6253e
zengyq <1244934835@qq.com>
2026-06-28 15:15:26 +0800

    refactor(sidepanel): 移除 App 顶部 Header 组件

--- entrypoints/sidepanel/App.tsx
+++ entrypoints/sidepanel/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import Header from './components/Header';
 import TabBar, { type Tab } from './components/TabBar';
 import SiteTools from './pages/SiteTools';
 import KeywordTools from './pages/KeywordTools';
@@ -8,7 +7,6 @@ export default function App() {
   const [tab, setTab] = useState<Tab>('site');
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-      <Header />
       <TabBar tab={tab} onChange={setTab} />
       <main style={{ flex: 1, minWidth: 0, overflowY: 'auto' }}>
         {tab === 'site' ? <SiteTools /> : <KeywordTools />}
